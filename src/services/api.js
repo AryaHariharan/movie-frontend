617c903e2188aed3ab59882a29d07654f968d94b
AryaHariharan <aryahariharannair@gmail.com>
2026-05-06 14:44:37 +0530

    connected to live backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080"
+  baseURL: "https://bookmyshow-l6oi.onrender.com"
 });
 
 // ✅ Attach JWT token automatically
